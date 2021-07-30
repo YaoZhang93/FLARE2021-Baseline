9ebdd3e714c10266296d8f11362919a310fbda60
Jun <1259389904@qq.com>
2021-07-30 10:09:09 +0800

    Update predict.sh

--- FLARE21/predict.sh
+++ FLARE21/predict.sh
@@ -1,10 +1,6 @@
 # !/bin/bash -e
 
-export CUDA_VISIBLE_DEVICES=1
 export nnUNet_raw_data_base="$PWD"
-# path to preprocessing results
-# export nnUNet_preprocessed="/home/an/nnUNet/datasets/Task101_preprocessed/"
-# path to segmentation results
 export RESULTS_FOLDER="$PWD/datasets/"
 
 nnUNet_predict -i "/workspace/inputs/"  -o "/workspace/outputs/"  -t Task000_FLARE21Baseline -f all --disable_tta -tr nnUNetTrainerV2 -m 3d_fullres
